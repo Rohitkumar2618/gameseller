1fd89274bc31efc722e38810c62385a768740b5e
Rohitkumar2618 <rohitkumarmankar26.akrk@gmail.com>
2024-04-10 15:13:03 +0530

    build the color mode switcher

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,8 @@ function App() {
       <NavBar />
     </GridItem>
 
-    <Show above="lg"> <GridItem area='aside' bg='coral' >Aside</GridItem> </Show>
-    <GridItem area='main' bg='dodgerblue' >Main</GridItem>
+    <Show above="lg"> <GridItem area='aside'  >Aside</GridItem> </Show>
+    <GridItem area='main' >Main</GridItem>
 
 
   </Grid>
