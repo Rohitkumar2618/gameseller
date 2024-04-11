6f1269a8a3713a69fe73a3409258b3617877e6e4
Rohitkumar2618 <rohitkumarmankar26.akrk@gmail.com>
2024-04-11 18:03:18 +0530

    error detected

--- src/App.tsx
+++ src/App.tsx
@@ -12,13 +12,22 @@ function App() {
     base: `"nav" "main"`,
     lg: `"nav nav""aside main"`, // big screen
 
-  }}>
+  }}
+
+    templateColumns={{
+      base: "1fr",
+      lg: "200px 1fr", // big screen
+    }}
+
+
+
+  >
 
     <GridItem area='nav'   >
       <NavBar />
     </GridItem>
 
-    <Show above="lg"> <GridItem area='aside'  >
+    <Show above="lg"> <GridItem area='aside' paddingX={5}  >
       <GenreList />
     </GridItem> </Show>
     <GridItem area='main' >
