ddcb53780e185ef3e128a889d9652e072bc36ba1
Rohitkumar2618 <rohitkumarmankar26.akrk@gmail.com>
2024-04-11 18:40:18 +0530

    Showing a snipper while fetching the genres

--- src/Components/GameGrid.tsx
+++ src/Components/GameGrid.tsx
@@ -16,7 +16,7 @@ const GameGrid = () => {
             <SimpleGrid
                 columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
                 padding="10px"
-                spacing={3}
+                spacing={10}
             >
                 {isLoading ? (
                     skeletons.map((skeleton) => (
--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,38 +1,37 @@
+import { AxiosRequestConfig, CanceledError } from "axios";
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
-import { AxiosError } from "axios";
 
 interface FetchResponse<T> {
     count: number;
     results: T[];
 }
 
-const useData = <T>(endpoint: string) => {
+const useData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig, deps?: any[]) => {
     const [data, setData] = useState<T[]>([]);
-    const [error, setError] = useState<string>('');
+    const [error, setError] = useState("");
     const [isLoading, setLoading] = useState(false);
 
     useEffect(() => {
         const controller = new AbortController();
-        setLoading(true);
 
-        apiClient.get<FetchResponse<T>>(endpoint, { signal: controller.signal })
-            .then(res => {
+        setLoading(true);
+        apiClient
+            .get<FetchResponse<T>>(endpoint, { signal: controller.signal, ...requestConfig })
+            .then((res) => {
                 setData(res.data.results);
                 setLoading(false);
             })
-            .catch((err: AxiosError) => {
-                if (err.name === 'AbortError') return; // Check for abort error
-                setError(err.message);
+            .catch((err) => {
+                if (err instanceof CanceledError) return;
+                setError(err.message)
                 setLoading(false);
             });
 
-        return () => {
-            controller.abort();
-        };
-    }, [endpoint]); // Dependency on endpoint to rerun effect when it changes
+        return () => controller.abort();
+    }, deps ? [...deps] : []);
 
     return { data, error, isLoading };
-}
+};
 
 export default useData;
