0c46b80ec293e38eca221e606fb793b5c02e5dde
Rohitkumar2618 <rohitkumarmankar26.akrk@gmail.com>
2024-04-13 17:11:44 +0530

    ready to deploy

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,5 +1,5 @@
 import { HStack, Image } from '@chakra-ui/react'
-import logo from '../assets/logo.webp'
+import logo from '../assets/download (2).png'
 import ColorModeSwitch from './ColorModeSwitch'
 import SearchInput from './SearchInput'
 
@@ -10,7 +10,7 @@ interface Props {
 const NavBar: React.FC<Props> = ({ onSearch }) => { // Explicitly declare Props type for better clarity
     return (
         <HStack padding="1 0px">
-            <Image src={logo} boxSize='60px' />
+            <Image src={logo} boxSize='120px' />
             {/* Pass onSearch directly */}
             <SearchInput onSearch={onSearch} />
             <ColorModeSwitch />
